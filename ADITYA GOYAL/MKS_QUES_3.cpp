24a438782b96dce3f0e9a6edd98e11e9770551cc
Aditya Goyal <adityagoyal680@gmail.com>
2022-05-16 10:09:44 +0530

    ASSIGNMENT COMPLETED

---
+++ ADITYA GOYAL/MKS_QUES_3.cpp
@@ -0,0 +1,7 @@
+QUES-3	How you store negative number in programming (you can use c++ for implementation) 
+		and if you print always positive number what will you use rather than default condition
+
+ANS-3	The simplest is to simply use the leftmost digit of the number as a special value to 
+		represent the sign of the number: 0 = positive, 1 = negative. For example, a value of 
+		positive 12 (decimal) would be written as 01100 in binary, but negative 12 (decimal) 
+		would be written as 11100.
